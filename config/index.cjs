c3420ab92409df7ac0d32fd46697e9bb1223054b
Weilong Dong <51409295+willowdong@users.noreply.github.com>
2023-07-25 21:42:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqZTcw5GJBIb5HdOT8EPj_PRoPmM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '8rW8xaU9KS3oWuk-xzej6JdhFCVE2bFaAfPon8Vi6ls',
+      useTemplateId: 'PgiK-ZRhUdVNrAQU_0tsl7PiPYqhGvHpJ-9OSbVRNfs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-15',
       festivals: [
