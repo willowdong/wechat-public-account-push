91274bc3d443ebe15d7d624b8327aa05ec5d1a69
Weilong Dong <51409295+willowdong@users.noreply.github.com>
2023-07-25 21:20:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -45,6 +45,34 @@ const USER_CONFIG = {
         { keyword: 'love_day', date: '2023-05-13' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oqZTcw_lslwzyOBm-Cg81UrVPqYI',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'vm9bVXUXGufQPHrcZAawodK7-Oe4aY9SsOKgezbp2LQ',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '03-15',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '2000', date: '02-10',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '宝贝', year: '2000', date: '03-15',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '08-18',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-05-13' },
+      ],
+    },
   ],
 
 
